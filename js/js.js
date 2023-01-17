05315ad33e800ba8e325e2bfd6b16ee503df9260
ivan <ivanlatorre720@gmail.com>
2023-01-16 23:01:51 -0300

    agrengado un for

--- js/js.js
+++ js/js.js
@@ -1,32 +1,114 @@
 document.getElementById('primary').addEventListener('click',function(){
     document.getElementById('izquierda').style.backgroundColor = '#0d6efd';
+    document.getElementById('derecha').style.backgroundColor = 'white';
+    
+    document.getElementById("nombre").style.color = 'white';
+    
+    document.getElementsByClassName("titulo")[0].style.color = 'white';
+    for( i = 0; i <= 4; i++){
+        document.getElementsByClassName("texto")[i].style.color = 'white';
+    }
+
+
+
 });
 document.getElementById('secondary').addEventListener('click',function(){
     document.getElementById('izquierda').style.backgroundColor = '#6c757d';
+    document.getElementById('derecha').style.backgroundColor = 'white';
+
+
+
+
+
+
 });
 document.getElementById('success').addEventListener('click',function(){
     document.getElementById('izquierda').style.backgroundColor = '#198754';
+    document.getElementById('derecha').style.backgroundColor = 'white';
+
+
+
+
+
+
 });
 document.getElementById('danger').addEventListener('click',function(){
     document.getElementById('izquierda').style.backgroundColor = '#dc3545';
+    document.getElementById('derecha').style.backgroundColor = 'white';
+
+
+
+
+
+
 });
 document.getElementById('warning').addEventListener('click',function(){
     document.getElementById('izquierda').style.backgroundColor = '#ffc107';
+    document.getElementById('derecha').style.backgroundColor = 'white';
+
+
+
+
+
+
 });
+
+
+
 document.getElementById('info').addEventListener('click',function(){
     document.getElementById('izquierda').style.backgroundColor = '#0dcaf0';
+    document.getElementById('derecha').style.backgroundColor = 'white';
+
+
+
+
+
+
+
 });
 document.getElementById('light').addEventListener('click',function(){
     document.getElementById('izquierda').style.backgroundColor = '#f8f9fa';
+    document.getElementById('derecha').style.backgroundColor = 'beige';
+   
     document.getElementById("nombre").style.color = 'black';
-    document.getElementById("infoTitulo").style.color = 'black';
+    
+    document.getElementsByClassName("titulo")[0].style.color = 'black';
+    
     document.getElementsByClassName("texto")[0].style.color = 'black';
     document.getElementsByClassName("texto")[1].style.color = 'black';
     document.getElementsByClassName("texto")[2].style.color = 'black';
     document.getElementsByClassName("texto")[3].style.color = 'black';
     document.getElementsByClassName("texto")[4].style.color = 'black';
+    
+    
+    document.getElementsByClassName("titulo")[1].style.color = 'black';
+    
+    document.getElementsByClassName("texedu")[0].style.color = 'black';
+    document.getElementsByClassName("texedu")[1].style.color = 'black';
+    document.getElementsByClassName("texedu")[2].style.color = 'black';
+    document.getElementsByClassName("texedu")[3].style.color = 'black';
+    document.getElementsByClassName("texedu")[4].style.color = 'black';
+    document.getElementsByClassName("texedu")[5].style.color = 'black';
+   
+    
+    document.getElementsByClassName("titulo")[2].style.color = 'black';
+    
     document.getElementsByClassName("texto")[5].style.color = 'black';
+    document.getElementsByClassName("texto")[6].style.color = 'black';
+    document.getElementsByClassName("texto")[7].style.color = 'black';
+    
+    
+    document.getElementsByClassName("titulo")[3].style.color = 'black';
+
+       
 });
 document.getElementById('dark').addEventListener('click',function(){
     document.getElementById('izquierda').style.backgroundColor = '#212529';
+    document.getElementById('derecha').style.backgroundColor = 'white';
+
+
+
+
+
+
 });
